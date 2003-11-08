a391772268faf62c783de2e789b79fd9df527c51
jchiang <>
2003-11-08 21:36:26 +0000

    update obsSim application

--- src/test/obsSim.cxx
+++ src/test/obsSim.cxx
@@ -3,7 +3,7 @@
  * @brief A prototype O2 application.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/test/main.cxx,v 1.21 2003/10/30 16:18:54 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/test/obsSim.cxx,v 1.1 2003/11/07 02:50:52 jchiang Exp $
  */
 
 #ifdef TRAP_FPE
@@ -78,8 +78,9 @@ int main(int iargc, char * argv[]) {
 
 // Generate the events and spacecraft data.
    bool useGoodi = false;
-   observationSim::EventContainer events("test_events", useGoodi);
-   observationSim::ScDataContainer scData("test_scData", useGoodi);
+   std::string prefix = params.string_par("Output_file_prefix");
+   observationSim::EventContainer events(prefix + "_events", useGoodi);
+   observationSim::ScDataContainer scData(prefix + "_scData", useGoodi);
 
 // The spacecraft object.
    observationSim::Spacecraft *spacecraft = new observationSim::LatSc();
@@ -87,11 +88,11 @@ int main(int iargc, char * argv[]) {
 // Use simulation time rather than total counts if desired.
    if (useSimTime) {
       std::cout << "Generating events for a simulation time of "
-                << count << " seconds....";
+                << count << " seconds...." << std::endl;
       my_simulator.generateEvents(static_cast<double>(count), events, 
                                   scData, respPtrs, spacecraft);
    } else {
-      std::cout << "Generating " << count << " events....";
+      std::cout << "Generating " << count << " events...." << std::endl;
       my_simulator.generateEvents(count, events, scData, respPtrs, spacecraft);
    }
    std::cout << "Done." << std::endl;
