98750cd26f0c3bd986034f88b5e8511f6fae1386
jchiang <>
2007-10-27 05:59:55 +0000

    read LIVETIME when supplied with an input FT2 file and us computed livetime fraction

--- src/LatSc.cxx
+++ src/LatSc.cxx
@@ -3,8 +3,11 @@
  * @brief Implementation of LatSc class.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/LatSc.cxx,v 1.20 2007/10/22 15:25:56 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/LatSc.cxx,v 1.21 2007/10/23 23:32:30 jchiang Exp $
  */
+
+#include "tip/IFileSvc.h"
+#include "tip/Table.h"
 
 #include "astro/EarthCoordinate.h"
 #include "astro/PointingTransform.h"
@@ -13,6 +16,20 @@
 #include "LatSc.h"
 
 namespace observationSim {
+
+LatSc::LatSc(const std::string & ft2file) : Spacecraft() {
+   const tip::Table * scData = 
+      tip::IFileSvc::instance().readTable(ft2file, "SC_DATA");
+   tip::Table::ConstIterator it(scData->begin());
+   tip::ConstTableRecord & row(*it);
+   for ( ; it != scData->end(); ++it) {
+      m_start.push_back(row["START"].get());
+      double duration(row["STOP"].get() - m_start.back());
+      m_livetimefrac.push_back(row["livetime"].get()/duration);
+   }
+   m_dt = m_start.at(1) - m_start.at(0);
+   delete scData;
+}
 
 astro::SkyDir LatSc::zAxis(double time) {
    HepRotation rotationMatrix = InstrumentToCelestial(time);
@@ -70,6 +87,30 @@ void LatSc::getZenith(double time, double & ra, double & dec) {
    astro::SkyDir zenithDir(gps->zenithDir());
    ra = zenithDir.ra();
    dec = zenithDir.dec();
+}
+
+double LatSc::livetimeFrac(double time) const {
+   if (m_start.size() == 0 || time < m_start.front() || time > m_start.back()) {
+      return Spacecraft::livetimeFrac(time);
+   }
+   size_t indx = static_cast<size_t>((time - m_start.front())/m_dt);
+   if (m_start.at(indx) <= time && time < m_start.at(indx+1)) {
+      return m_livetimefrac.at(indx);
+   }
+// Intervals are not uniform, so must do a search.  The offsets from
+// the computed index should be constant and small over large ranges,
+// so a linear search from the computed point is reasonable.
+   if (m_start.at(indx) > time) {
+      while (m_start.at(indx) > time && indx > 0) {
+         indx--;
+      }
+      return m_livetimefrac.at(indx);
+   }
+// This is the only case left, so use it as default.
+   while (m_start.at(indx) < time && indx < m_start.size()) {
+      ++indx;
+   }
+   return m_livetimefrac.at(indx);
 }
 
 } // namespace observationSim
