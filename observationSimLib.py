766991bd88dd6d876329f57e3ba4cebfe37e8b2a
cvs2git <>
2008-02-26 05:14:33 +0000

    This commit was manufactured by cvs2svn to create tag 'observationSim-07-02-01'.
    Sprout from master 2008-01-08 22:19:59 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2008-02-26 05:14:32 UTC glastrm 'Set lib dependencies up for when building libraries':
        SConscript
        observationSimLib.py

--- observationSimLib.py
+++ observationSimLib.py
@@ -1,5 +1,7 @@
+#$Id$
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['observationSim'], package = 'observationSim')
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['observationSim'])
     env.Tool('facilitiesLib')
     env.Tool('tipLib')
     env.Tool('astroLib')
