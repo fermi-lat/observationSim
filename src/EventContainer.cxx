6aa3c658276530dde78a950deb5356dab7f505ed
jchiang <>
2008-05-13 21:26:01 +0000

    back off on using new event class defs

--- src/EventContainer.cxx
+++ src/EventContainer.cxx
@@ -4,7 +4,7 @@
  * when they get written to a FITS file.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/EventContainer.cxx,v 1.88 2008/03/14 05:20:47 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/observationSim/src/EventContainer.cxx,v 1.89 2008/03/25 03:58:15 jchiang Exp $
  */
 
 #include <cmath>
@@ -244,10 +244,11 @@ void EventContainer::writeEvents(double obsStopTime) {
       ft1["phi"].set(evt->phi());
       ft1["zenith_angle"].set(evt->zenAngle());
       ft1["earth_azimuth_angle"].set(earthAzimuthAngle(ra, dec, time));
-      ft1["event_class"].set(evt->eventClass());
+//
+// Not ready for this change yet.
+//      ft1["event_class"].set(evt->eventClass());
+      ft1["event_class"].set(evt->conversionType());
       ft1["conversion_type"].set(evt->conversionType());
-//       ft1["event_class"].set(evt->conversionType());
-//       ft1["conversion_type"].set(evt->conversionType());
       ft1["mc_src_id"].set(evt->eventId());
       ft1["mcenergy"].set(evt->trueEnergy());
    }
